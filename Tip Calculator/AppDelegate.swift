0ccec3f9b061a3111b7c614bf94bae011d0b8360
Raghav Nyati <raghavnyati90@gmail.com>
2017-08-17 17:57:26 -0700

    Default navigation bar theme and tint added in app delegate.

--- Tip Calculator/AppDelegate.swift
+++ Tip Calculator/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+       
+        let tintColor = UIColor.brown
+        window!.tintColor = tintColor
+        
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.brown]
+        UINavigationBar.appearance().tintColor = UIColor.brown
         
         return true
     }
